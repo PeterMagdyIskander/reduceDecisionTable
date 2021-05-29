32edd295303f8fe5c625a70f8873e363d6c2ab87
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2021-05-29 17:17:10 +0200

    working code on college test case

--- src/reducingtable/Reducingtable.java
+++ src/reducingtable/Reducingtable.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Reducingtable {
 
     public static Hashtable<String, ArrayList<String>> reducedTable = new Hashtable<>();
-    
+
     static ArrayList<String> getInput() {
         ArrayList<String> conditionsAndOutput = new ArrayList<>();
         conditionsAndOutput.add("F,F,E");
@@ -48,81 +48,85 @@ public class Reducingtable {
         return sameAction;
     }
 
-    static void reduction(int compareSize, String Letter,int size) {
-        int[]falseIndices=new int[size];
-        
-        ////ignore=2
-        for(int i =0;i<size;i++)
-            falseIndices[i]=2;
+    static void reduction(int conditionNumber, String Letter, int size) {
+        int[] falseIndices = new int[size];
+        ////initial 
         ArrayList<ArrayList<Integer>> indexToMakeDash = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            falseIndices[i] = 2;
+        }
         Hashtable<String, ArrayList<String>> sameAction = makeHashtable();
         ArrayList<String> bigArr = sameAction.get(Letter);
         for (int i = 0; i < bigArr.size(); i++) {
-            String[] compare = bigArr.get(i).split(",");
-            for (int j = i + 1; j < bigArr.size(); j++) {
-                int counter = 0;
-                String[] compare2 = bigArr.get(j).split(",");
-                for (int k = 0; k < compare.length; k++) {
-                    if (compare[k].equals(compare2[k])) {
-                        counter++;
-                    }
-                }
-                if (counter == compareSize) {
-                    //change = 1
-                    //ignore and remove = 0
-                    falseIndices[i]=1;
-                    falseIndices[j]=0;
-                    i = i + 2;
-                    for (int k = 0; k < compare.length; k++) {
+            if (falseIndices[i] == 2) {
+                String[] compare = bigArr.get(i).split(",");
+                for (int j = i + 1; j < bigArr.size(); j++) {
+                    if (falseIndices[j] == 2) {
+                        int counter = 0;
+                        String[] compare2 = bigArr.get(j).split(",");
                         ArrayList<Integer> toBeAddedToList = new ArrayList<>();
-                        if (compare[k] != compare2[k]) {
-
-                            toBeAddedToList.add(k);
-
+                        for (int k = 0; k <= conditionNumber - 1; k++) {
+                            if (compare[k].equals(compare2[k])) {
+                                counter++;
+                            } else {
+                                toBeAddedToList.add(k);
+                            }
                         }
-                        indexToMakeDash.add(toBeAddedToList);
+                        if (counter == conditionNumber - 1) {
+                            //change = 1
+                            //ignore and remove = 0
+                            //System.out.println(i);
+                            falseIndices[i] = 1;
+                            falseIndices[j] = 0;
+                            indexToMakeDash.add(toBeAddedToList);
+                            break;
+                        }else{
+                            falseIndices[i] = 2;
+                            toBeAddedToList.clear();
+                            indexToMakeDash.add(toBeAddedToList);
+                        }
                     }
-                }else{
-                    ArrayList<Integer> toBeAddedToList = new ArrayList<>();
-                    toBeAddedToList.add(0);
-                    indexToMakeDash.add(toBeAddedToList);
                 }
             }
+
         }
-        merge(falseIndices,indexToMakeDash,Letter);
+        merge(falseIndices, indexToMakeDash, Letter);
     }
-    static void merge(int[]falseIndices,ArrayList<ArrayList<Integer>> indexToMakeDash,String Letter){
+
+    static void merge(int[] falseIndices, ArrayList<ArrayList<Integer>> indexToMakeDash, String Letter) {
         Hashtable<String, ArrayList<String>> sameAction = makeHashtable();
         ArrayList<String> bigArr = sameAction.get(Letter);
-        ArrayList<String> newSameAction= new ArrayList<>();
-        for(int i =0;i<falseIndices.length;i++){
-            if(falseIndices[i]==1){
-                String newString=bigArr.get(i);
-                char dash='-';
-               StringBuilder sb=new StringBuilder(newString);
-                for(int j=0;j<indexToMakeDash.get(i).size();j++){
+        ArrayList<String> newSameAction = new ArrayList<>();
+        for (int i = 0; i < falseIndices.length; i++) {
+            if (falseIndices[i] == 1) {
+                String newString = bigArr.get(i);
+                char dash = '-';
+                StringBuilder sb = new StringBuilder(newString);
+                for (int j = 0; j < indexToMakeDash.get(i).size(); j++) {
                     sb.setCharAt(indexToMakeDash.get(i).get(j), dash);
-                    newString=sb.toString();
+                    newString = sb.toString();
                 }
-            newSameAction.add(newString);
-            }else if(falseIndices[i]==2){
-                String newString=bigArr.get(i);
+                newSameAction.add(newString);
+            } else if (falseIndices[i] == 2) {
+                String newString = bigArr.get(i);
                 newSameAction.add(newString);
             }
         }
         reducedTable.put(Letter, newSameAction);
     }
     public static void main(String[] args) {
-        reduction(1,"H",1);
-        reduction(1,"E",3);
+        reduction(1, "H", 1);
+        reduction(2, "E", 3);
         ArrayList<String> arr = reducedTable.get("E");
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i));
         }
+        
         System.out.println("-------------------------------------");
         arr = reducedTable.get("H");
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i));
         }
+        
     }
 }
