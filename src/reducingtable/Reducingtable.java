2413ed8aa5920bf7cef45a7082ffc3159b2e918b
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2021-05-29 19:09:37 +0200

    only the input is missing

--- src/reducingtable/Reducingtable.java
+++ src/reducingtable/Reducingtable.java
@@ -21,41 +21,49 @@ public class Reducingtable {
 
     static ArrayList<String> getInput() {
         ArrayList<String> conditionsAndOutput = new ArrayList<>();
-        conditionsAndOutput.add("F,F,E");
-        conditionsAndOutput.add("T,F,E");
-        conditionsAndOutput.add("F,T,E");
-        conditionsAndOutput.add("T,T,H");
+        conditionsAndOutput.add("PE,=<75,-,C");
+        conditionsAndOutput.add("PE,=<75,-,C");
+        conditionsAndOutput.add("PE,>75,-,DC");
+        conditionsAndOutput.add("PE,>75,-,DC");
+
+        conditionsAndOutput.add("PA,-,Y,C");
+        conditionsAndOutput.add("PA,-,N,DC");
+        conditionsAndOutput.add("PA,-,Y,C");
+        conditionsAndOutput.add("PA,-,N,DC");
+
         return conditionsAndOutput;
     }
 
-    static ArrayList<String> getArr(String action) {
+    static ArrayList<String> getArr(int numOfConditions, String action) {
         ArrayList<String> conditionsAndOutput = getInput();
         ArrayList<String> allConditions = new ArrayList<>();
         for (int i = 0; i < conditionsAndOutput.size(); i++) {
-            if (conditionsAndOutput.get(i).endsWith(action)) {
+            String[] key = conditionsAndOutput.get(i).split(",");
+            if (key[numOfConditions].equals(action)) {
                 allConditions.add(conditionsAndOutput.get(i));
             }
         }
         return allConditions;
     }
 
-    static Hashtable<String, ArrayList<String>> makeHashtable() {
+    static Hashtable<String, ArrayList<String>> makeHashtable(int numOfConditions) {
         ArrayList<String> conditionsAndOutput = getInput();
         Hashtable<String, ArrayList<String>> sameAction = new Hashtable<>();
         for (int i = 0; i < conditionsAndOutput.size(); i++) {
-            sameAction.put(conditionsAndOutput.get(i).substring(4), getArr(conditionsAndOutput.get(i).substring(4)));
+            String[] key = conditionsAndOutput.get(i).split(",");
+            sameAction.put(key[numOfConditions], getArr(numOfConditions, key[numOfConditions]));
         }
         return sameAction;
     }
 
-    static void reduction(int conditionNumber, String Letter, int size) {
+    static void reduction(int numOfConditions, String Letter, int size) {
         int[] falseIndices = new int[size];
         ////initial 
         ArrayList<ArrayList<Integer>> indexToMakeDash = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             falseIndices[i] = 2;
         }
-        Hashtable<String, ArrayList<String>> sameAction = makeHashtable();
+        Hashtable<String, ArrayList<String>> sameAction = makeHashtable(numOfConditions);
         ArrayList<String> bigArr = sameAction.get(Letter);
         for (int i = 0; i < bigArr.size(); i++) {
             if (falseIndices[i] == 2) {
@@ -65,22 +73,24 @@ public class Reducingtable {
                         int counter = 0;
                         String[] compare2 = bigArr.get(j).split(",");
                         ArrayList<Integer> toBeAddedToList = new ArrayList<>();
-                        for (int k = 0; k <= conditionNumber - 1; k++) {
-                            if (compare[k].equals(compare2[k])) {
+                        for (int k = 0; k <= numOfConditions - 1; k++) {
+                            if (compare[k].equals(compare2[k]) && !(compare[k].equals("-") && compare2[k].equals("-"))) {
                                 counter++;
+
                             } else {
                                 toBeAddedToList.add(k);
                             }
                         }
-                        if (counter == conditionNumber - 1) {
+                        if (counter == numOfConditions - 1) {
                             //change = 1
                             //ignore and remove = 0
-                            //System.out.println(i);
+                            if(falseIndices[i]==1){
+                                continue;
+                            }
                             falseIndices[i] = 1;
                             falseIndices[j] = 0;
                             indexToMakeDash.add(toBeAddedToList);
-                            break;
-                        }else{
+                        } else {
                             falseIndices[i] = 2;
                             toBeAddedToList.clear();
                             indexToMakeDash.add(toBeAddedToList);
@@ -90,11 +100,11 @@ public class Reducingtable {
             }
 
         }
-        merge(falseIndices, indexToMakeDash, Letter);
+        merge(numOfConditions, falseIndices, indexToMakeDash, Letter);
     }
 
-    static void merge(int[] falseIndices, ArrayList<ArrayList<Integer>> indexToMakeDash, String Letter) {
-        Hashtable<String, ArrayList<String>> sameAction = makeHashtable();
+    static void merge(int numOfConditions, int[] falseIndices, ArrayList<ArrayList<Integer>> indexToMakeDash, String Letter) {
+        Hashtable<String, ArrayList<String>> sameAction = makeHashtable(numOfConditions);
         ArrayList<String> bigArr = sameAction.get(Letter);
         ArrayList<String> newSameAction = new ArrayList<>();
         for (int i = 0; i < falseIndices.length; i++) {
@@ -106,6 +116,7 @@ public class Reducingtable {
                     sb.setCharAt(indexToMakeDash.get(i).get(j), dash);
                     newString = sb.toString();
                 }
+
                 newSameAction.add(newString);
             } else if (falseIndices[i] == 2) {
                 String newString = bigArr.get(i);
@@ -114,19 +125,22 @@ public class Reducingtable {
         }
         reducedTable.put(Letter, newSameAction);
     }
+
     public static void main(String[] args) {
-        reduction(2, "H", 1);
-        reduction(2, "E", 3);
-        ArrayList<String> arr = reducedTable.get("E");
+        reduction(3, "C", 4);
+        reduction(3, "DC", 4);
+
+        ArrayList<String> arr = reducedTable.get("C");
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i));
         }
-        
+
         System.out.println("-------------------------------------");
-        arr = reducedTable.get("H");
+
+        arr = reducedTable.get("DC");
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i));
         }
-        
+
     }
 }
