f62f184c30e4370a725caab0a6e019b91c2f16e2
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2021-05-29 11:52:36 +0200

    initial commit

---
+++ src/reducingtable/Reducingtable.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reducingtable;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+/**
+ *
+ * @author Not a gaming laptop
+ */
+public class Reducingtable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numberOfconditions=3;
+        int numberOfActions=1;
+        Hashtable<String, ArrayList<String>> sameAction = new Hashtable<>();
+        
+        
+    }
+    public void KEKL(){
+        ArrayList<String>conditionsAndOutput=new ArrayList<>();
+        conditionsAndOutput.add("F,F,E");
+        conditionsAndOutput.add("T,F,E");
+        conditionsAndOutput.add("F,T,E");
+        conditionsAndOutput.add("T,T,H");
+        
+        
+    }
+}
